983859c032c2857c35ae1f562b9bbec1e6b1bedd
Rick Stanley <rick-stanley@outlook.com>
2020-08-21 16:17:25 -0400

    mixin

--- src/components/Checklist.js
+++ src/components/Checklist.js
@@ -1,44 +1,60 @@
 /** @typedef {import('./CheckItem.js').default} CheckItem */
 
-// @todo Document mixin
-/** 
- * @type {OrderedChecklist & UnorderedChecklist}
- */
-const sharedMethods = {
+const sharedMethodsMixin = Base => class Shared extends Base {
+
+  constructor() {
+    super();
+  }
+
+  /** @return {CheckItem[] | null} */
+  get items() {
+    return Array.from(this.querySelectorAll('check-item'));
+  }
+
   getCheckedItems() {
     return this.items.filter(item => item.checked);
-  },
+  }
+
   getUncheckedItems() {
     return this.items.filter(item => !item.checked);
-  },
+  }
+
+  static fromJSON(items, type) {
+    switch (type) {
+      case 'ol':
+        return OrderedChecklist.createList(items);
+      case 'ul':
+        return UnorderedChecklist.createList(items);
+      default:
+        return items.map(Shared._createItem).join('');
+    }
+  }
+
+  static _createItem(item) {
+    const hasName = 'name' in item;
+    const isChecked = 'checked' in item;
+    return `<li>${'items' in item ? `${item.content}${Shared.fromJSON(item.items, item.type)}` : `<check-item ${hasName ? `name="${item.name}"` : ''} ${isChecked ? 'checked' : ''}>${item.content}</check-item>`}</li>`;
+  }
 };
 
-class OrderedChecklist extends HTMLOListElement {
+class OrderedChecklist extends sharedMethodsMixin(HTMLOListElement) {
   static DEFAULT_NAME = 'ordered-checklist';
-  static EXTENDS = 'ol';
+  static TYPE = 'ol';
 
-  /** @type {CheckItem[] | null} */
-  items = Array.from(this.querySelectorAll('check-item')) || [];
-
-  constructor() {
-    super();
+  static createList(items) {
+    return `<ol is="${OrderedChecklist.DEFAULT_NAME}" class="list lateral-padding">${items.map(super._createItem).join('')}</ol>`;
   }
 }
 
-class UnorderedChecklist extends HTMLUListElement {
+class UnorderedChecklist extends sharedMethodsMixin(HTMLUListElement) {
   static DEFAULT_NAME = 'unordered-checklist';
-  static EXTENDS = 'ul';
+  static TYPE = 'ul';
 
-  /** @type {CheckItem[] | null} */
-  items = Array.from(this.querySelectorAll('check-item')) || [];
-
-  constructor() {
-    super();
+  static createList(items) {
+    return `<ul is="${UnorderedChecklist.DEFAULT_NAME}" class="list lateral-padding">${items.map(super._createItem).join('')}</ul>`;
   }
 }
 
-Object.assign(OrderedChecklist.prototype, sharedMethods);
-Object.assign(UnorderedChecklist.prototype, sharedMethods);
 
 export {
   OrderedChecklist,
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { UnorderedChecklist } from "./components/CheckList.js";
 import CircularProgress from "./components/CircularProgress.js";
 
 customElements.define(CheckItem.DEFAULT_NAME, CheckItem);
-customElements.define(UnorderedChecklist.DEFAULT_NAME, UnorderedChecklist, { extends: UnorderedChecklist.EXTENDS });
+customElements.define(UnorderedChecklist.DEFAULT_NAME, UnorderedChecklist, { extends: UnorderedChecklist.TYPE });
 customElements.define(CircularProgress.DEFAULT_NAME, CircularProgress);
 
 const ready = f => /in/.test(document.readyState) ? setTimeout(ready, 5, f) : f();
@@ -19,17 +19,80 @@ ready(() => {
 
   /** @type {UnorderedChecklist} */
   const checkList = document.querySelector(`[is=${UnorderedChecklist.DEFAULT_NAME}]`);
-  const checkItemsLength = checkList.items.length;
 
   const progress = document.querySelector(CircularProgress.DEFAULT_NAME, CircularProgress);
 
   questUpdateSound.play();
 
+  const items = [
+    {
+      content: 'Hello',
+      checked: true,
+    },
+    {
+      content: 'Hi there',
+    },
+    {
+      content: 'Malganisss',
+    },
+    {
+      content: 'My sublist',
+      type: 'ol',
+      items: [
+        {
+          content: 'My sub item 1',
+          checked: true,
+        },
+        {
+          content: 'My sub item 2',
+          checked: true,
+        },
+      ]
+    },
+    {
+      content: 'Hello guys'
+    },
+    {
+      content: 'My sublist 2',
+      type: 'ul',
+      items: [
+        {
+          content: 'My sub item 3',
+          checked: true,
+        },
+        {
+          content: 'My sub item 4',
+          checked: true,
+        },
+      ]
+    },
+    {
+      content: 'Malganisss',
+    },
+    {
+      content: 'Malganisss',
+    },
+  ];
+
+  checkList.innerHTML = UnorderedChecklist.fromJSON(items);
+
+  const checkItemsLength = checkList.items.length;
+
+  const updateProgress = () => {
+    const totalComplete = Math.floor(checkList.getCheckedItems().length / checkItemsLength * 100);
+    progress.value = totalComplete;
+
+    return totalComplete;
+  };
+
+  updateProgress();
+
   checkList.addEventListener('change', event => {
     if (event.target.checked)
       skillIncreaseSound.play();
-    const totalComplete = checkList.getCheckedItems().length / checkItemsLength * 100;
-    progress.value = totalComplete;
+
+    const totalComplete = updateProgress();
+
     if (totalComplete === 100) {
       levelUpSound.play();
     }
